449cc924eec18add655826b503db2a97e7a9277c
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-12 23:25:17 -0400

    feat: update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Mercadito for you!</h1>`,
+  imports: [HeaderComponent, HomeComponent],
+  template: `
+    <app-header (search)="query = $event"></app-header>
+    <app-home [query]="query"></app-home>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
+  query = '';
 }
