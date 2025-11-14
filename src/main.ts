6087cd932b2893873478fa5380e2b9b972b34e81
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-14 01:25:19 -0400

    feat: add provideHttpClient and Router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimations()],
+  providers: [provideAnimations(), provideHttpClient(), provideRouter(appRoutes)],
 })
   .catch(err => console.error(err));
